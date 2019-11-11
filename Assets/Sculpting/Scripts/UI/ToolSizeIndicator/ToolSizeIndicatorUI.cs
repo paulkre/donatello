6a5a42031873dc0ae1c60f877821c16c82d9ff5d
Paul Kretschel <paul.kretschel@icloud.com>
2019-11-11 16:38:24 +0100

    Implement tool size indicator for Odyssey

--- Assets/Sculpting/Scripts/UI/ToolSizeIndicator/ToolSizeIndicatorUI.cs
+++ Assets/Sculpting/Scripts/UI/ToolSizeIndicator/ToolSizeIndicatorUI.cs
@@ -6,12 +6,16 @@ namespace VRSculpting.UI.ToolSizeIndicator {
 	[RequireComponent(typeof(LineRenderer))]
 	public class ToolSizeIndicatorUI : UI {
 
-		private static int resolution = 32;
+		private static int resolution = 64;
 		private static float step = (2 * Mathf.PI) / resolution;
-		private static float lineWidth = .005f;
+		private static float lineWidth = .001f;
+		private static float activeDuration = .5f;
 
 		private LineRenderer lineRenderer;
 		private Vector3[] points;
+
+		private float alpha = 0.0f;
+		private float lastChange;
 
 		private float Radius {
 			set {
@@ -36,11 +40,16 @@ namespace VRSculpting.UI.ToolSizeIndicator {
 			Radius = menu.ToolSize.Value / 2;
 			menu.ToolSize.OnChange += (param, lastValue) => {
 				Radius = param.Value / 2;
+				lastChange = Time.time;
 			};
 		}
 
 		private void Update() {
 			transform.rotation = Quaternion.LookRotation(Camera.main.transform.position - transform.position);
+
+			float targetAlpha = Time.time - lastChange <= activeDuration ? 1f : 0f;
+			alpha += (targetAlpha - alpha) * .25f;
+			lineRenderer.startColor = lineRenderer.endColor = new Color(1, 1, 1, alpha);
 		}
 
 	}
