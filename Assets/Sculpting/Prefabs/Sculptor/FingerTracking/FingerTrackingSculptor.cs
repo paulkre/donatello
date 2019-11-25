9e229204e0c359329f64d8c2d185ea7ad3021ec2
Paul Kretschel <paul.kretschel@icloud.com>
2019-11-25 19:41:06 +0100

    Add finger tracking sculptor and scene

---
+++ Assets/Sculpting/Prefabs/Sculptor/FingerTracking/FingerTrackingSculptor.cs
@@ -0,0 +1,27 @@
+﻿using FingerTracking;
+using FingerTracking.UI.Picker;
+
+namespace VRSculpting.Sculptor.FingerTracking
+{
+
+    public class FingerTrackingSculptor : SculptorBehaviour
+    {
+        public TrackedHand rightHand;
+        public PickerBehaviour picker;
+
+        protected override SculptState GetState(SculptState prev)
+        {
+            return new SculptState
+            {
+                position = rightHand.transform.position,
+                strength = 1f,
+
+                drawing = picker.State,
+                drawingDown = picker.StateDown,
+                drawingUp = picker.StateUp,
+                drawingInverted = false,
+            };
+        }
+
+    }
+}
