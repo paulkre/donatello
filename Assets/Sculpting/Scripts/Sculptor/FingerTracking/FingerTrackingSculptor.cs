a67f9be851e5f290885edbb710a40cb98c0f39e9
Paul Kretschel <paul.kretschel@icloud.com>
2019-11-26 17:46:35 +0100

    Enable both hands for finger tracking

--- Assets/Sculpting/Scripts/Sculptor/FingerTracking/FingerTrackingSculptor.cs
+++ Assets/Sculpting/Scripts/Sculptor/FingerTracking/FingerTrackingSculptor.cs
@@ -1,5 +1,8 @@
 ﻿using FingerTracking;
 using FingerTracking.UI.Picker;
+using FingerTracking.UI.FingerSlider;
+using VRSculpting.SculptMesh.Modification;
+using VRSculpting.Settings;
 
 namespace VRSculpting.Sculptor.FingerTracking
 {
@@ -7,7 +10,28 @@ namespace VRSculpting.Sculptor.FingerTracking
     public class FingerTrackingSculptor : SculptorBehaviour
     {
         public TrackedHand rightHand;
-        public PickerBehaviour picker;
+
+        public PickerBehaviour pickerPrefab;
+        public FingerSliderBehaviour sliderPrefab;
+
+        private PickerBehaviour picker;
+        private FingerSliderBehaviour slider;
+
+        public override void Init(SculptMesh.Modification.SculptMesh sculptMesh, Menu menu)
+        {
+            base.Init(sculptMesh, menu);
+
+            picker = Instantiate(pickerPrefab, transform);
+            picker.hand = rightHand;
+
+            slider = Instantiate(sliderPrefab, transform);
+            slider.hand = rightHand;
+
+            slider.OnChange += (value, lastValue) =>
+            {
+                menu.SelectedParameter.RelativeValue += value - lastValue;
+            };
+        }
 
         protected override SculptState GetState(SculptState prev)
         {
