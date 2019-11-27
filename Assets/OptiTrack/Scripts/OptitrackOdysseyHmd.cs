ce650982be2377572dca20646265e72f3848f6ee
Paul Kretschel <paul.kretschel@icloud.com>
2019-11-27 16:36:35 +0100

    Fix OptiTrack offset for Odyssey HMD

---
+++ Assets/OptiTrack/Scripts/OptitrackOdysseyHmd.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.XR;
+
+public class OptitrackOdysseyHmd : MonoBehaviour
+{
+    public Transform hmdRigidbody;
+    public Transform positionOffset;
+
+    void Update()
+    {
+        var hmdPosition = InputTracking.GetLocalPosition(XRNode.CenterEye);
+
+        positionOffset.position = hmdRigidbody.position - hmdPosition;
+    }
+}
